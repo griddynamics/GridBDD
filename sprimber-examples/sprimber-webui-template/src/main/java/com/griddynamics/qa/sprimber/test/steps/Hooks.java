103d354d615cb0709935deb3492e63a16c394a16
Piotr Michalski <pmichalski@kohls.com>
2020-03-23 14:14:39 +0100

    [Issue #66] Template for WebUI testing with Selenium

---
+++ sprimber-examples/sprimber-webui-template/src/main/java/com/griddynamics/qa/sprimber/test/steps/Hooks.java
@@ -0,0 +1,33 @@
+package com.griddynamics.qa.sprimber.test.steps;
+
+import com.griddynamics.qa.sprimber.engine.model.action.Actions;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.qameta.allure.AllureLifecycle;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+@Actions
+@RequiredArgsConstructor
+@Slf4j
+public class Hooks {
+
+    private final AllureLifecycle allureLifecycle;
+    private final WebDriver webDriver;
+
+    @AfterStep
+    public void attachScreenShot() {
+        allureLifecycle.addAttachment("Screenshot after test step", "image/png", "png",
+                ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES));
+    }
+
+    // TODO: change to @AfterFailure once it's implemented
+    @After
+    public void after() {
+        allureLifecycle.addAttachment("Page source after failure", "text/plain", "html",
+                webDriver.getPageSource().getBytes());
+    }
+}
---
+++ sprimber-examples/sprimber-webui-template/src/main/resources/feature/GetInTouch.feature
@@ -0,0 +1,6 @@
+Feature: WebUI Template suite - 'Get in Touch' page
+
+  @getInTouch @failed
+  Scenario: Fail if link is not clicked
+    Given open main GridDynamics page
+    Then 'Get in Touch' page is opened
